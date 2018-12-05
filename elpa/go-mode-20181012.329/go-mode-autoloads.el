564dd811497a6f5acbf83bde40fe234ae32a8e09
guodongsoft <guodongsoft@163.com>
2018-12-05 23:39:05 +0900

    package install

--- elpa/go-mode-20181012.329/go-mode-autoloads.el
+++ elpa/go-mode-20181012.329/go-mode-autoloads.el
@@ -100,10 +100,6 @@ Tries to look for a URL at point.
 
 ;;;***
 
-;;;### (autoloads nil nil ("go-mode-pkg.el") (0 0 0 0))
-
-;;;***
-
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
