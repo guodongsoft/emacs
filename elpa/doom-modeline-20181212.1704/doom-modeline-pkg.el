7494dd81ac7b3a702386b89ced9c64198a1012b3
dong guo <dong.guo@mobilus.co.jp>
2018-12-14 17:52:11 +0900

    package update

---
+++ elpa/doom-modeline-20181212.1704/doom-modeline-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "doom-modeline" "20181212.1704" "A minimal and modern modeline" '((emacs "25.1") (all-the-icons "1.0.0") (shrink-path "0.2.0") (eldoc-eval "0.1") (dash "2.11.0")) :commit "14ac6ddbeb411665dc7058a696a2bc7f94e22846" :keywords '("faces" "mode-line") :authors '(("Vincent Zhang" . "seagle0128@gmail.com")) :maintainer '("Vincent Zhang" . "seagle0128@gmail.com") :url "https://github.com/seagle0128/doom-modeline")
