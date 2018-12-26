81395f2eb208e1e64a344a9fe59c4dd310ff862d
dong guo <dong.guo@mobilus.co.jp>
2018-12-26 20:25:09 +0900

    add term color

--- lisp/custom-term.el
+++ lisp/custom-term.el
@@ -148,6 +148,19 @@
 
 ;; (lazyload (multi-term multi-term-dedicated-open) "multi-term" nil)
 
+(custom-set-faces
+ '(term-color-black ((t (:foreground "#3F3F3F" :background "#2B2B2B"))))
+ '(term-color-red ((t (:foreground "#AC7373" :background "#8C5353"))))
+ '(term-color-green ((t (:foreground "#7F9F7F" :background "#9FC59F"))))
+ '(term-color-yellow ((t (:foreground "#DFAF8F" :background "#9FC59F"))))
+ '(term-color-blue ((t (:foreground "#7CB8BB" :background "#4C7073"))))
+ '(term-color-magenta ((t (:foreground "#DC8CC3" :background "#CC9393"))))
+ '(term-color-cyan ((t (:foreground "#93E0E3" :background "#8CD0D3"))))
+ '(term-color-white ((t (:foreground "#DCDCCC" :background "#656555"))))
+
+ '(term-default-fg-color ((t (:inherit term-color-white))))
+ '(term-default-bg-color ((t (:inherit term-color-black)))))
+
 (add-hook 'term-mode-hook
           '(lambda ()
              ;;  mkdir -p ~/.terminfo
