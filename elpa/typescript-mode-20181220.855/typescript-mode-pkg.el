9d32a405faa8b07c968886477186a1e8f41ea60e
guodongsoft <guodongsoft@163.com>
2018-12-27 05:30:46 +0900

    update

--- elpa/typescript-mode-20181220.855/typescript-mode-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "typescript-mode" "20181220.855" "Major mode for editing typescript" 'nil :keywords
-  '("typescript" "languages")
-  :url "http://github.com/ananthakumaran/typescript.el")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
