90920c685e50a24b43b4a59c3f9249d47024cfaa
dong guo <dong.guo@mobilus.co.jp>
2019-01-08 16:32:56 +0900

    modeline表示変更

--- lisp/custom-modeline.el
+++ lisp/custom-modeline.el
@@ -8,31 +8,19 @@
 
 ;;; Code:
 ;; 文字コード表示
-(defvar my-mode-line-coding-format
-  '(:eval
-    (let* ((code (symbol-name buffer-file-coding-system))
-           (eol-type (coding-system-eol-type buffer-file-coding-system))
-           (eol (if (eq 0 eol-type) "UNIX"
-                  (if (eq 1 eol-type) "DOS"
-                    (if (eq 2 eol-type) "MAC"
-                      "???")))))
-      (concat code " " eol " "))))
-(put 'my-mode-line-coding-format 'risky-local-variable t)
-
 (doom-modeline-init)
 (setq doom-modeline-buffer-file-name-style 'file-name)
 (setq-default mode-line-format
  (list
   '(:propertize " %l/%c " face (:weight bold))
-  'my-mode-line-coding-format
   'mode-line-modified
   'mode-line-remote " "
   '(:eval
     (propertize " %b "
      'face
      (if (buffer-modified-p)
-      '(:background "#FF00FF" :foreground "#FDF6E3" :weight bold)
-      '(:background "#FF00FF" :foreground "#FDF6E3" :weight normal))))
+      '(:background "#B7CEEB" :foreground "#8A2BE2" :weight bold)
+      '(:background "#B7CEEB" :foreground "#404040" :weight normal))))
   '(:propertize " %p/%I " face (:background "#8A2BE2" :foreground "#FDF6E3"))
   '(:eval (propertize (concat " " (eyebrowse-mode-line-indicator) " ")))
   '(:eval
@@ -44,7 +32,8 @@
   '(:propertize vc-mode
     face
     (:inherit 'font-lock-keyword-face :background "#8B8386" :foreground "#76EE00" :weight bold))
-  " %m "))
+  '(:propertize " %m " face (:background "#8A2BE2" :foreground "#FDF6E3"))
+  '(:eval (propertize (concat " " (symbol-name buffer-file-coding-system) " ")))))
 
 (provide 'custom-modeline)
 ;;; custom-modeline.el ends here
